53d1742c4f39c5f170c75360ac1b4ab57ae375be
Julian <40236707+juleznakamoto@users.noreply.github.com>
2025-09-17 05:55:18 +0000

    Restored to '6514031bb197167248445ff078ee6696664da797'
    Replit-Restored-To: 6514031bb197167248445ff078ee6696664da797

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,7 @@ export default {
             height: "0",
           },
         },
+        },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
