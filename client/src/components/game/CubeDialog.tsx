6fbf3601f22715b90bf5691671b78a1feb14ce49
Julian <40236707+juleznakamoto@users.noreply.github.com>
2025-10-06 18:30:47 +0000

    Restored to 'fa52c006a6a13be3d57d8f640b2443de49551c3b'
    Replit-Restored-To: fa52c006a6a13be3d57d8f640b2443de49551c3b

--- client/src/components/game/CubeDialog.tsx
+++ client/src/components/game/CubeDialog.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { LogEntry } from "@/game/rules/events";
 import {
@@ -26,7 +27,7 @@ export default function CubeDialog({
 
   return (
     <Dialog open={isOpen} onOpenChange={() => {}}>
-      <DialogContent className="[&>button]:border-2 [&>button]:border-gray-400 [&>button]:rounded-none border-4 border-gray-400 rounded-none shadow-2xl p-6 !w-[28rem] !h-[28rem] max-w-[28rem] max-h-[28rem] flex flex-col cube-dialog-glow">
+      <DialogContent className="[&>button]:hidden border-2 border-gray-400 rounded-none shadow-2xl p-6 !w-[20rem] !h-[20rem] max-w-[20rem] max-h-[20rem] flex flex-col">
         <DialogHeader className="flex-shrink-0">
           <DialogTitle className="text-lg font-semibold">
             {event.title || "Strange Encounter"}
