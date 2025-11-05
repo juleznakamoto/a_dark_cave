82858a6bb7d3a8b04580f5b5910373fb6f8c3705
Julian <40236707+juleznakamoto@users.noreply.github.com>
2025-11-05 11:39:43 +0000

    Restored to '613580e09c05483fda0b6788cd3670392f5c1cb0'
    Replit-Restored-To: 613580e09c05483fda0b6788cd3670392f5c1cb0

--- client/src/components/ui/animated-shader-hero.tsx
+++ client/src/components/ui/animated-shader-hero.tsx
@@ -472,9 +472,9 @@ uniform float time;
 
 // COLOR CUSTOMIZATION - Modify these values to change the color scheme
 // Each vec3 represents RGB values (0.0 to 1.0)
-#define PRIMARY_COLOR vec3(1.0, 0.6, 0.2)      // Bright orange fire
-#define SECONDARY_COLOR  vec3(1.0, 0.4, 0.0)      // Orange-red
-#define TERTIARY_COLOR vec3(1.0, 0.8, 0.0)     // Yellow-orange
+#define PRIMARY_COLOR vec3(0.7, 0.7, 0.7)      // Orange-red (default)
+#define SECONDARY_COLOR  vec3(0.8, 0.2, 1.0)      // Purple
+#define TERTIARY_COLOR vec3(1.0, 0.5, 0.0)     // Bright orange
 #define BACKGROUND_TINT vec3(0.025, 0.025, 0.025) // Dark orange-brown
 
 // COLOR DEVIATION - Controls how much colors can deviate from PRIMARY_COLOR
