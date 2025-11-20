a2a57e64a0baf3b1a1c6e8a924c6ca94e7e543fc
Julian <40236707+juleznakamoto@users.noreply.github.com>
2025-11-20 19:31:49 +0000

    Restored to 'b1a749c55ab8535cd86970b8258ee506d9995769'
    Replit-Restored-To: b1a749c55ab8535cd86970b8258ee506d9995769

--- client/src/components/game/panels/ForestPanel.tsx
+++ client/src/components/game/panels/ForestPanel.tsx
@@ -164,7 +164,7 @@ export default function ForestPanel() {
       );
 
       return upgradeKey ? (
-        <div key={actionId} className="relative inline-block overflow-visible">
+        <div key={actionId} className="relative inline-block">
           {button}
           <ButtonLevelBadge upgradeKey={upgradeKey} />
         </div>
@@ -187,7 +187,7 @@ export default function ForestPanel() {
     );
 
     return upgradeKey ? (
-      <div key={actionId} className="relative inline-block overflow-visible">
+      <div key={actionId} className="relative inline-block">
         {button}
         <ButtonLevelBadge upgradeKey={upgradeKey} />
       </div>
