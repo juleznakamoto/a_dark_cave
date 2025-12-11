49e453cbd8baf020d4984ac9f30bb438cf01b5a9
Julian <40236707+juleznakamoto@users.noreply.github.com>
2025-12-11 09:20:16 +0000

    Restored to '27ffa69b8d3e32c4bcf0550916acd4459eddfd64'
    Replit-Restored-To: 27ffa69b8d3e32c4bcf0550916acd4459eddfd64

--- client/src/components/game/IdleModeDialog.tsx
+++ client/src/components/game/IdleModeDialog.tsx
@@ -450,7 +450,7 @@ export default function IdleModeDialog() {
 
   return (
     <Dialog open={idleModeDialog.isOpen} onOpenChange={() => {}}>
-      <DialogContent className="sm:max-w-sm" hideClose={true} hideOverlay={true}>
+      <DialogContent className="sm:max-w-sm" hideClose={true}>
         <DialogHeader>
           <DialogTitle >Sleeping</DialogTitle>
           <DialogDescription className = "py-1Cu">
