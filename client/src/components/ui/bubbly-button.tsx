31fc59c86bdf27a2e2bbf44ad932fb311f401330
Julian <40236707+juleznakamoto@users.noreply.github.com>
2026-02-13 06:16:42 +0000

    styling

--- client/src/components/ui/bubbly-button.tsx
+++ client/src/components/ui/bubbly-button.tsx
@@ -61,11 +61,8 @@ const BUILD_TONES = [
 export const CRAFT_TONES = [
   tailwindToHex("amber-900"),
   tailwindToHex("amber-950"),
-  tailwindToHex("yellow-600"),
   tailwindToHex("yellow-900"),
   tailwindToHex("orange-950"),
-  tailwindToHex("red-600"),
-  tailwindToHex("red-900"),
   tailwindToHex("red-950"),
 ];
 
